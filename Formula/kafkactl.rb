c6c70bf5636145bc041c1a81c66266e8634d0ac1
Jimmy Bonds <jbonds@jbvm.io>
2021-02-18 11:49:39 -0500

    remove devel block

--- Formula/kafkactl.rb
+++ Formula/kafkactl.rb
@@ -7,13 +7,6 @@ class Kafkactl < Formula
   if OS.mac?
     url "https://github.com/jbvmio/kafkactl/releases/download/v1.0.26/kafkactl_1.0.26_Darwin_x86_64.tar.gz"
     sha256 "d41f344db6604479e822df30fc6c16e61ec4b655cb3b6f0651fbc598e9297f42"
-
-  devel do
-    url "https://github.com/jbvmio/kafkactl/releases/download/v1.0.22-beta.12858318/kafkactl_1.0.22-beta.12858318_Darwin_x86_64.tar.gz"
-    version "1.0.22-beta.12858318"
-    sha256 "ef16cf3dee6afc23ebe5ac8505af3e1b08dcb1022c0808cdfbc3fa6d1f96ca1f"
-  end
-
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/jbvmio/kafkactl/releases/download/v1.0.26/kafkactl_1.0.26_Linux_x86_64.tar.gz"
