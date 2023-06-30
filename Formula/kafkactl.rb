ffba89b5612af38c0055feca02bd05150346efb5
Jimmy Bonds <jbonds@jbvm.io>
2023-06-30 16:45:40 -0400

    update formula

--- Formula/kafkactl.rb
+++ Formula/kafkactl.rb
@@ -3,19 +3,13 @@ class Kafkactl < Formula
   desc "CLI for Apache Kafka and Zookeeper Management."
   homepage "http://www.jbvm.io/kafkactl"
   version "1.0.26"
-  bottle :unneeded
-  if OS.mac?
-    url "https://github.com/jbvmio/kafkactl/releases/download/v1.0.26/kafkactl_1.0.26_Darwin_x86_64.tar.gz"
-    sha256 "d41f344db6604479e822df30fc6c16e61ec4b655cb3b6f0651fbc598e9297f42"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/jbvmio/kafkactl/releases/download/v1.0.26/kafkactl_1.0.26_Linux_x86_64.tar.gz"
-      sha256 "3fe58121d52eb56ced4423fa88de453775ffc57adc5206023276a97367feb855"
-    end
-  end
+  url "https://github.com/jbvmio/kafkactl/releases/download/v1.0.26/kafkactl_1.0.26_Darwin_x86_64.tar.gz"
+  sha256 "d41f344db6604479e822df30fc6c16e61ec4b655cb3b6f0651fbc598e9297f42"
+
   def install
     bin.install "kafkactl"
   end
+
   test do
     system "#{bin}/kafkactl version"
   end
